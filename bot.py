afa283a18b4f6a3449797c6f95713a02a1e3e1d5
JordiAlonsoLlovet <38110017+JordiAlonsoLlovet@users.noreply.github.com>
2019-12-23 15:53:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import random
+import praw
 import sys
 
 from telegram.ext import Updater, CommandHandler
@@ -10,9 +10,14 @@ logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
+#Creating a read-only Reddit instance
+reddit = praw.Reddit(client_id='DhuZOjJy8UoelQ',
+                     client_secret='xX_cP0bUNdxiBwxXlStUNAuZUh8',
+                     user_agent='windows:telegramBot:v.1.0.0 (by /u/top-reddit-bot)')
+
 # Getting mode, so we could define run function for local and Heroku setup
-mode = os.getenv("MODE")
-TOKEN = os.getenv("TOKEN")
+mode = sys.argv[1]
+TOKEN = sys.argv[2]
 if mode == "dev":
     def run(updater):
         updater.start_polling()
@@ -33,14 +38,18 @@ else:
 def start_handler(bot, update):
     # Creating a handler-function for /start command 
     logger.info("User {} started bot".format(update.effective_user["id"]))
-    update.message.reply_text("Hello from Python!\nPress /random to get random number")
+    update.message.reply_text("Welcome to TopRedditBot! This bot allows you to find today's top post for your favourite subreddits\nPress Type /reddit <subreddit> to get ")
 
 
-def random_handler(bot, update):
-    # Creating a handler-function for /random command
-    number = random.randint(0, 10)
-    logger.info("User {} randomed number {}".format(update.effective_user["id"], number))
-    update.message.reply_text("Random number: {}".format(number))
+def reddit_handler(bot, update):
+    request= update.message.text.split(" ", 1)
+    subreddit = request[-1].split("/")
+    logger.info("Searching for {} subreddit".format(subreddit))
+    try:
+        for submission in reddit.subreddit(subreddit[-1]).top('day',limit=1):
+            update.message.reply_text(submission.url)
+    except:
+        update.message.reply_text("Oops, this subreddit doesn't exist or has restricted acces. Please try another one")
 
 
 if __name__ == '__main__':
@@ -48,6 +57,6 @@ if __name__ == '__main__':
     updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
+    updater.dispatcher.add_handler(CommandHandler("reddit", reddit_handler))
 
     run(updater)
